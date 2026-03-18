55a6a99fc01d53823a810c72451fa1dd8920ae18
Aleksey Andreev <andreev-krdr@yandex.ru>
2026-03-18 10:34:56 +0300

    поправил замечания

--- tests/fixture.py
+++ tests/fixture.py
@@ -1,6 +1,8 @@
 import random
 import pytest
+from locator import *
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 
 URL = "https://stellarburgers.education-services.ru/"
 
@@ -15,3 +17,28 @@ def open_main_page():
     yield driver
     driver.quit()
 
+@pytest.fixture
+def registered_user():
+    email = generate_email()
+    password = '123456'
+    name = 'Newuser1'
+
+    driver = webdriver.Chrome()
+    driver.get(URL)
+
+    driver.find_element(*ENTER_IN_BUTTON).click()
+    driver.find_element(*REG_BUTTON).click()
+    driver.find_element(*USERNAME_FIELD).send_keys(name)
+    driver.find_element(*EMAIL_FIELD).send_keys(email)
+    driver.find_element(*PASSWORD_FIELD).send_keys(password)
+    driver.find_element(*LOGIN_BUTTON).click()
+
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(ENTER_BUTTON_ENTER))
+
+    driver.quit()
+
+    return {
+        'email': email,
+        'password': password,
+        'name': name
+    }
